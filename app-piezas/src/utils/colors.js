5e528b31eace2569acf30a5b6eda28f78e777f58
MigueUrrutia <163359725+MigueUrrutia@users.noreply.github.com>
2025-04-03 21:58:28 -0600

    Creacion de colors.js

---
+++ app-piezas/src/utils/colors.js
@@ -0,0 +1,12 @@
+export default {
+    primary: '#1976D2',       
+    secondary: '#90CAF9',    
+    accent: '#FF6F00',       
+    background: '#F5F5F5',    
+    text: '#212121',          
+    textLight: '#757575',     
+    danger: '#D32F2F',        
+    white: '#FFFFFF',         
+    border: '#BDBDBD'         
+  };
+  
